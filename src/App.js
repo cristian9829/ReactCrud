7bbb6e77f1a3716102dbc6a8bef379c5eab9c988
Cristian Clavijo <cristian982940@gmail.com>
2019-05-30 00:16:28 -0500

    create component Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/header';
 import Productos from './components/productos';
 import Producto from './components/producto';
 import EditarProducto from './components/editarProducto';
@@ -9,12 +10,16 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
+        <main className="container mt-5">
         <Switch>
           <Route exact path="/nuevo-producto" component={AgregarProducto} />
           <Route exact path="/productos" component={Productos} />
           <Route exact path="/producto/:id" component={Producto} />
           <Route exact path="/productos/editar/:id" component={EditarProducto } />
         </Switch>
+        </main>
+        <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
       </Router>
     </div>
   );
